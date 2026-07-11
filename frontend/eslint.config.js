d1da23a3c36b0908e9dd39adab759d24b3512327
avramenkomy <avramenkomy@yandex.ru>
2026-07-11 12:39:31 +0300

    chore: configure react refresh for mobx observer

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -8,6 +8,15 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+          extraHOCs: ['observer'],
+        },
+      ],
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
