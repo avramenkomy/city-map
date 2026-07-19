ea476b0245f3227db2768ab69748acab7236acbe
avramenkomy <avramenkomy@yandex.ru>
2026-07-19 12:59:58 +0300

    chore: add test dependences and update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+  },
   server: {
     proxy: {
       '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/media': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
       }
